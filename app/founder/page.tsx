36c02d9325973301d6776478702df1cd2c8010c6
226275 <140880384+226275@users.noreply.github.com>
2026-01-15 00:50:55 +0530

    Create page.tsx

---
+++ app/founder/page.tsx
@@ -0,0 +1,9 @@
+import { FounderSection } from "@/components/founder-section"
+
+export default function FounderPage() {
+  return (
+    <main className="min-h-screen">
+      <FounderSection />
+    </main>
+  )
+}
